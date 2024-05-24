24fe7cce8d0b6b59a8de267ef4ab157aa8624756
PeterAMassih <peteabdelmassih@gmail.com>
2024-05-24 13:16:34 +0200

    sft training

--- training_sft/sft_train.py
+++ training_sft/sft_train.py
@@ -1,84 +1,68 @@
-import json
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
-from peft import get_peft_model, LoraConfig, TaskType
+from trl import SFTTrainer
+from peft import LoraConfig
 import os
+import torch
 
-os.environ["HF_TOKEN"] = "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"
+access_token = "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"
+os.environ["HF_TOKEN"] = access_token
 
-output_file = 'M1_processed_data.json'
+model_id = "meta-llama/Meta-Llama-3-8B"
 
-# Load the processed dataset
-dataset = load_dataset('json', data_files=output_file)
+# Check CUDA availability
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Training on {device}")
 
-# Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+dataset = load_dataset("json", data_files="M1_sft.json", split="train")
+dataset = dataset.train_test_split(test_size=0.1)  # 10% of the data will be used for testing
+train_dataset = dataset['train']
+test_dataset = dataset['test']
 
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
+tokenizer = AutoTokenizer.from_pretrained(model_id, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2")
+model = AutoModelForCausalLM.from_pretrained(model_id)
 
-# Set max_length
-max_length = 8192  # Adjust based on your model's capability
+tokenizer.pad_token = tokenizer.eos_token
 
-# Tokenize the dataset
-def tokenize_function(examples):
-    inputs = tokenizer(examples['input'], padding="max_length", truncation=True, max_length=max_length)
-    labels = tokenizer(examples['label'], padding="max_length", truncation=True, max_length=max_length)
-    return {
-        "input_ids": inputs["input_ids"], 
-        "attention_mask": inputs["attention_mask"], 
-        "labels": labels["input_ids"]
-    }
-
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
-
-# Split the dataset
-split_datasets = tokenized_dataset['train'].train_test_split(test_size=0.1)
-train_dataset = split_datasets['train']
-eval_dataset = split_datasets['test']
-
-# Load the model
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
-
-# Define LoRA configuration
-lora_config = LoraConfig(
-    task_type=TaskType.CAUSAL_LM,
-    r=32,  # Adjust this value based on your specific needs and GPU capacity
+peft_config = LoraConfig(
+    r=8,
     lora_alpha=32,
-    lora_dropout=0.1,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=["q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj"]
 )
 
-# Apply LoRA to the model
-model = get_peft_model(model, lora_config)
-
-# Define training arguments
 training_args = TrainingArguments(
-    output_dir='./checkpoints',
+    output_dir="./checkpoints",
+    evaluation_strategy="epoch",
+    save_total_limit=5,
+    save_steps=500,
+    gradient_accumulation_steps=4,
+    optim = "adamw_8bit",
+    gradient_checkpointing=True,
+    bf16=True,
+    logging_dir=f"./logs",  # TensorBoard logging directory
+    logging_steps=10,  # Adjust the frequency of logging
+    learning_rate=2e-5,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=1,
     num_train_epochs=5,
-    per_device_train_batch_size=2,  # Adjust batch size based on available GPU memory
-    per_device_eval_batch_size=2,
-    evaluation_strategy='epoch',
-    save_strategy='epoch',
-    logging_dir='./logs',
-    logging_steps=10,
-    learning_rate=5e-5,
-    save_total_limit=2,
-    load_best_model_at_end=True,
-    metric_for_best_model='eval_loss',
-    greater_is_better=False, ## because using eval loss
+    weight_decay=0.01,
+    report_to="tensorboard",  # Enable TensorBoard reporting
 )
 
-# Initialize the Trainer
-trainer = Trainer(
+trainer = SFTTrainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
+    eval_dataset=test_dataset,
     tokenizer=tokenizer,
+    peft_config=peft_config
 )
 
 # Train the model
 trainer.train()
-
-# Save the fine-tuned model
-trainer.save_model('./fine_tuned_model_lora')
