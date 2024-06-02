2c0e27932d3a9372c147e6b1fba4cdc1d9669442
PeterAMassih <peteabdelmassih@gmail.com>
2024-06-02 17:41:15 +0200

    merge updated

--- training_sft/merge.py
+++ training_sft/merge.py
@@ -1,4 +1,5 @@
 from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
 
 # Load PEFT model on CPU
 model = AutoPeftModelForCausalLM.from_pretrained(
@@ -10,3 +11,11 @@ model = AutoPeftModelForCausalLM.from_pretrained(
 merged_model = model.merge_and_unload()
 new_model_name = "LLama-3-8B-SFT-4-pack"
 merged_model.save_pretrained(new_model_name, safe_serialization=True, max_shard_size="5GB")
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained("./best_models/checkpoint_sft")
+
+# Save the tokenizer to the same directory as the merged model
+tokenizer.save_pretrained(new_model_name)
+
+print(f"Model and tokenizer saved to {new_model_name}")
