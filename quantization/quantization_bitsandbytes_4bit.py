d8298942c6a2e68c4bc42e548db581eb88afd18e
PeterAMassih <peteabdelmassih@gmail.com>
2024-06-11 19:36:34 +0200

    Moved 4bit script + changed token

--- quantization/quantization_bitsandbytes_4bit.py
+++ quantization/quantization_bitsandbytes_4bit.py
@@ -1,18 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
-from datasets import load_dataset, concatenate_datasets
-import wandb
 import torch
 import os 
-import json
-
-os.environ["WANDB_PROJECT"] = "mnlp"  # name your W&B project
-os.environ["WANDB_LOG_MODEL"] = "checkpoint"  # log all model checkpoints
-
-# Initialize WandB
-wandb.init(project="mnlp")
 
 # Set up access token and model ID
-access_token = "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"
+access_token = "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"
 os.environ["HF_TOKEN"] = access_token
 model_id = "PeterAM4/EPFL-TA-Meister"
 
@@ -22,14 +13,16 @@ print(f"Training on {device}")
 
 # BitsAndBytesConfig int-4 config
 bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+    load_in_4bit=True,                    # Load the model in 4-bit
+    bnb_4bit_use_double_quant=True,       # Use double quantization
+    bnb_4bit_quant_type="nf4",            # Use the nf4 quantization type weights from normal distribution
+    bnb_4bit_compute_dtype=torch.bfloat16 # Use bfloat16 for computation of the quantized weights
 )
 
 # Load model and tokenizer
 quantized_model = AutoModelForCausalLM.from_pretrained(
     model_id,
     device_map="auto",
-    attn_implementation="flash_attention_2",
     torch_dtype=torch.bfloat16,
     quantization_config=bnb_config
 )
@@ -37,8 +30,8 @@ quantized_model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.pad_token = tokenizer.eos_token
 
-# save the quantize model to disk
-save_folder = "checkpoints/quantized_llama_2"
-quantized_model.save_pretrained(save_folder, safe_serialization=True)
+# Push the quantized model to the Hugging Face Hub
+quantized_model.push_to_hub("PeterAM4/EPFL-TA-Meister-4bit")
+tokenizer.push_to_hub("PeterAM4/EPFL-TA-Meister-4bit")
 
 
