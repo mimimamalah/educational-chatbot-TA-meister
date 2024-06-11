70c98ae241ff60dda874df804b4f2644cb4963a8
PeterAMassih <peteabdelmassih@gmail.com>
2024-06-11 18:53:17 +0200

    Added writing token

--- quantization/quantization_bitsandbytes_8bit.py
+++ quantization/quantization_bitsandbytes_8bit.py
@@ -1,16 +1,16 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import os
 
-# Set up your Hugging Face API token this is a write token
+# Set up your Hugging Face API token
 os.environ["HF_TOKEN"] = "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"
 
 # Define the configuration for 8-bit quantization with an outlier threshold
 quantization_config = BitsAndBytesConfig(
     load_in_8bit=True,
-    llm_int8_threshold=6.0  # Set outlier threshold
+    llm_int8_threshold=6.0  # Set outlier threshold to 6.0 changed to fp16
 )
 
-# Load the model with quantization
+# Load the model with quantization high precision layers are still in fp32
 model = AutoModelForCausalLM.from_pretrained(
     "PeterAM4/EPFL-TA-Meister", 
     quantization_config=quantization_config
