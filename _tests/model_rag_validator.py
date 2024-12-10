daf3b90eadb39dabc77c188caea7f90c793e2473
Malak Lahlou Nabil <106238482+mimimamalah@users.noreply.github.com>
2024-12-10 11:40:28 +0100

    Delete _tests directory

--- _tests/model_rag_validator.py
+++
@@ -1,8 +0,0 @@
-IMPLEMENTING_RAG = True
-
-import os
-
-if IMPLEMENTING_RAG:
-    assert os.path.isdir("model/documents"), "You must have a directory named 'documents' with all the documents used for RAG."
-
-print('Model documents directory exists for the RAG implementation.')
